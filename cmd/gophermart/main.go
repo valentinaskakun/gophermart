94c9f8c8b45f2c29dba03c513aca1f4fe7ae5882
valentina.skakun <valentina.skakun@inn.ru>
2022-08-19 10:35:07 +0300

    branch4 2

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/jwtauth/v5"
-	"github.com/zamibaru89/gophermart/internal/config"
-	"github.com/zamibaru89/gophermart/internal/functions"
-	"github.com/zamibaru89/gophermart/internal/handlers"
-	"github.com/zamibaru89/gophermart/internal/storage"
 	"log"
 	"net/http"
-	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
+
+	"github.com/zamibaru89/gophermart/internal/config"
+	"github.com/zamibaru89/gophermart/internal/handlers"
+	"github.com/zamibaru89/gophermart/internal/storage"
 )
 
 var tokenAuth *jwtauth.JWTAuth
@@ -30,16 +30,16 @@ func main() {
 		return
 
 	}
-	tickerUpdate := time.NewTicker(10 * time.Second)
-	go func() {
-		for range tickerUpdate.C {
-			//log.Println("start AccrualUpdate")
-			err := functions.AccrualUpdate(Server, ServerConfig)
-			if err != nil {
-				log.Println(err)
-			}
-		}
-	}()
+	//tickerUpdate := time.NewTicker(10 * time.Second)
+	//go func() {
+	//	for range tickerUpdate.C {
+	//		//log.Println("start AccrualUpdate")
+	//		err := functions.AccrualUpdate(Server, ServerConfig)
+	//		if err != nil {
+	//			log.Println(err)
+	//		}
+	//	}
+	//}()
 	r := chi.NewRouter()
 	r.Route("/api/user", func(r chi.Router) {
 		r.Group(func(r chi.Router) {
